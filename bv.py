1a70f1fa2dff1484dfc35154c68f07f63bb4fabb
Martin Vejnár <avakar@ratatanek.cz>
2015-12-22 23:33:20 +0100

    Support globs.

--- bv.py
+++ bv.py
@@ -1,4 +1,4 @@
-﻿import argparse, sys
+﻿import argparse, sys, glob
 from bv.parser import parse as parse_bv, parse_type
 from bv.ast import print_value
 from bv.gen_verilog import gen_verilog
@@ -8,8 +8,17 @@ if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('-m', '--module')
     p.add_argument('--print-ast', action='store_true')
-    p.add_argument('input', nargs='+', type=argparse.FileType('r'))
+    p.add_argument('input', nargs='+')
     args = p.parse_args()
+
+    new_inputs = []
+    for input_glob in args.input:
+        inputs = [open(fname, 'r') for fname in glob.iglob(input_glob)]
+        if not inputs:
+            print>>sys.stderr, 'error: not found: {}'.format(input_glob)
+            sys.exit(2)
+        new_inputs.extend(inputs)
+    args.input = new_inputs
 
     units = [parse_bv(input) for input in args.input]
     if args.print_ast:
